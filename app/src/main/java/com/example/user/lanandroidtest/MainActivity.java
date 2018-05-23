d1ab375ef2e6934f35e85bee066a37a48950caaf
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-05-23 16:33:21 -0400

    Basic broadcast reciever set up

--- app/src/main/java/com/example/user/lanandroidtest/MainActivity.java
+++ app/src/main/java/com/example/user/lanandroidtest/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.user.lanandroidtest;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final IntentFilter intentFilter = new IntentFilter();
+
+    private WifiP2pManager.Channel mChannel;
+    private WifiP2pManager mManager;
+
+    private BroadcastReceiver mReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    //Initializes necessary code to run the application
+    private void init(){
+
+        // Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        //Used tp connect to the P2P framework
+        mManager = (WifiP2pManager) getSystemService(this.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+
     }
 }
