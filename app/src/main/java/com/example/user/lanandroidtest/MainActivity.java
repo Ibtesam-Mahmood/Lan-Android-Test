688180c52f84ebdb5203ea74e501fc7e3002145b
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-05-27 22:40:27 -0400

    XML layout initialized

--- app/src/main/java/com/example/user/lanandroidtest/MainActivity.java
+++ app/src/main/java/com/example/user/lanandroidtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.lanandroidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Called when the refresh button is pressed
+    //Updates the peer list
+    public void refreshPeers(View view){
+
+
     }
 }
