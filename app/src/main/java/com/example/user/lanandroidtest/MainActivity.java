4a5f7e583475f13920f34e9fa181185e83966b0e
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-05-27 23:43:21 -0400

    Peer list display added

--- app/src/main/java/com/example/user/lanandroidtest/MainActivity.java
+++ app/src/main/java/com/example/user/lanandroidtest/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.user.lanandroidtest;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.adroitandroid.near.discovery.NearDiscovery;
 import com.adroitandroid.near.model.Host;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
     private static final long DISCOVERABLE_TIMEOUT_MILLIS = 60000;
     private static final long DISCOVERY_TIMEOUT_MILLIS = 10000;
     private static final long DISCOVERABLE_PING_INTERVAL_MILLIS = 5000;
+
     private NearDiscovery mNearDiscovery;
+
+    private LinearLayout ipLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +37,28 @@ public class MainActivity extends AppCompatActivity {
                 .setDiscoverablePingIntervalMillis(DISCOVERABLE_PING_INTERVAL_MILLIS)
                 .setDiscoveryListener(getNearDiscoveryListener(), Looper.getMainLooper())
                 .build();
+
+        ipLayout = findViewById(R.id.peerList);
     }
 
     //Called when the refresh button is pressed
     //Updates the peer list
     public void refreshPeers(View view){
 
+    }
+
+    //Displays the list of peers available to connect to
+    public void displayPeers(Host[] peers){
+
+        for(int i = 0; i < peers.length; i++){
+
+            TextView peer = new TextView(this);
+
+            peer.setText(peers[i].getName());
+
+            ipLayout.addView(peer);
+
+        }
 
     }
 
@@ -42,23 +66,26 @@ public class MainActivity extends AppCompatActivity {
         return new NearDiscovery.Listener() {
             @Override
             public void onPeersUpdate(Set<Host> hosts) {
-                // Handle updates of peer list here - some peer might have got removed if it wasn't reachable anymore or some new peer might have been added
+                //Obtains the list of peers and calls a method to display them
+                Host[] availablePeers = (Host[]) hosts.toArray();
+                displayPeers(availablePeers);
             }
 
             @Override
             public void onDiscoveryTimeout() {
-                // This is called after the discovery timeout (specified in the builder) from starting discovery using the startDiscovery()
+                Log.e("Peer", "Discovery Timeout");
             }
 
             @Override
             public void onDiscoveryFailure(Throwable e) {
-                // This is called if discovery could not be started
+                Log.e("Peer", "Something went wrong while searching for participants");
             }
 
             @Override
             public void onDiscoverableTimeout() {
-                // This is called after the discoverable timeout (specified in the builder) from becoming discoverable by others using the makeDiscoverable()
+                Log.e("Peer", "You're not discoverable anymore");
             }
+
         };
     }
 }
