84a806f886caf861cec3b08cf9af09f13764229b
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-06-01 04:53:03 -0400

    Salut callback implemented

--- app/src/main/java/com/example/user/lanandroidtest/MainActivity.java
+++ app/src/main/java/com/example/user/lanandroidtest/MainActivity.java
@@ -4,12 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.peak.salut.Callbacks.SalutDataCallback;
+
+public class MainActivity extends AppCompatActivity implements SalutDataCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onDataReceived(Object o) {
+        
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 
     //Called when the refresh button is pressed
