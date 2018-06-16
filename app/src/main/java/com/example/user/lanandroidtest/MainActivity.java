c361b485a10b42d7d32f3290c7cff2ad93185166
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-06-16 10:30:22 -0400

    Discovery code added

--- app/src/main/java/com/example/user/lanandroidtest/MainActivity.java
+++ app/src/main/java/com/example/user/lanandroidtest/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import java.io.IOException;
@@ -16,8 +17,12 @@ public class MainActivity extends AppCompatActivity {
 
     private int mLocalPort;
     private String mServiceName;
+    private String mServiceType;
+
+    private final String TAG = "NSD";
 
     private NsdManager.RegistrationListener mRegistrationListener;
+    private NsdManager.DiscoveryListener mDiscoveryListener;
 
     private NsdManager mNsdManager;
 
@@ -49,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         NsdServiceInfo serviceInfo = new NsdServiceInfo();
 
         serviceInfo.setServiceName("DND");
-        serviceInfo.setServiceName("_dnd._tcp");
+        serviceInfo.setServiceType("_dnd._tcp");
         serviceInfo.setPort(port);
 
         mNsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
@@ -58,14 +63,67 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //Initializes the discovery listener
     public void beginDiscovery(){
 
-
+        mNsdManager.discoverServices(mServiceType, NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
 
     }
 
     //Initializes the discovery listener
-    
+    public void initializeDiscoveryListener() {
+
+        // Instantiate a new DiscoveryListener
+        mDiscoveryListener = new NsdManager.DiscoveryListener() {
+
+            // Called as soon as service discovery begins.
+            @Override
+            public void onDiscoveryStarted(String regType) {
+                Log.d(TAG, "Service discovery started");
+            }
+
+            @Override
+            public void onServiceFound(NsdServiceInfo service) {
+                // A service was found! Do something with it.
+                Log.d(TAG, "Service discovery success" + service);
+                if (!service.getServiceType().equals(mServiceType)) {
+                    // Service type is the string containing the protocol and
+                    // transport layer for this service.
+                    Log.d(TAG, "Unknown Service Type: " + service.getServiceType());
+                } else if (service.getServiceName().equals(mServiceName)) {
+                    // The name of the service tells the user what they'd be
+                    // connecting to. It could be "Bob's Chat App".
+                    Log.d(TAG, "Same machine: " + mServiceName);
+                } else if (service.getServiceName().contains(mServiceName)){
+                    //mNsdManager.resolveService(service, mResolveListener);
+                }
+            }
+
+            @Override
+            public void onServiceLost(NsdServiceInfo service) {
+                // When the network service is no longer available.
+                // Internal bookkeeping code goes here.
+                Log.e(TAG, "service lost" + service);
+            }
+
+            @Override
+            public void onDiscoveryStopped(String serviceType) {
+                Log.i(TAG, "Discovery stopped: " + serviceType);
+            }
+
+            @Override
+            public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+                Log.e(TAG, "Discovery failed: Error code:" + errorCode);
+                mNsdManager.stopServiceDiscovery(this);
+            }
+
+            @Override
+            public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+                Log.e(TAG, "Discovery failed: Error code:" + errorCode);
+                mNsdManager.stopServiceDiscovery(this);
+            }
+        };
+    }
 
     //Initializes the registration listener
     public void initializeRegistrationListener() {
@@ -77,6 +135,8 @@ public class MainActivity extends AppCompatActivity {
                 // resolve a conflict, so update the name you initially requested
                 // with the name Android actually used.
                 mServiceName = NsdServiceInfo.getServiceName();
+                mServiceType = NsdServiceInfo.getServiceType();
+                beginDiscovery();
             }
 
             @Override
